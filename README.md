f8143e21c30104a15ac4c4562e4f76485216a8f0
Carlos <carlos.aponte@trivelope.de>
2023-01-27 16:30:34 +0100

    Handling of telegram commands

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # aoe2-taunt-bot
 
 Telegram bot that listens to a group or to direct messages and when it detects a valid AoE 2 taunt number, replies with the taunt's audio. 
+
+
+## How to use
+
+1.  Add the bot to your Telegram group by searching for its username.
+2.  Getting a Taunt. 
+	1. Send a valid AoE 2 taunt number as a regular message.
+	2. Type a valid AoE 2 taunt number as a command. **Ex: /8**
+	3. Use the bot inline to search for taunts to send. **Ex: @aoe2_taunts_bot 8**
+
+## List of valid taunt numbers
+
+The full list of AoE 2 taunts found in the [fandom](https://ageofempires.fandom.com/wiki/Taunts#Full_list_of_taunts).
+
+## Technical details
+
+This bot is built using Python and the [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot). The audio files are the same used in the fandom.
+
+## Note
+
+This bot is not affiliated or endorsed by Age of Empires 2 or its creators. The audio files used are property of Microsoft Corporation and are used only for the purpose of this bot.
--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
 
-from telegram import Update, InlineQueryResultAudio, InputTextMessageContent, InlineQueryResultArticle
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
+from telegram import Update, InlineQueryResultAudio
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, InlineQueryHandler
 
 from taunts import get_validated_taunt_tuple
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def taunt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
-       echo checks all messages send to the bot/group and if the message is a valid
-       AoE2 taunt number (ex: 1..105). The bot will reply with the audio file for that
+       checks all messages  and commands send to the bot or group and if it is a valid
+       AoE2 taunt number (1..105) the bot will reply with the audio file for that
        taunt.
        """
-
     message = update.message.text
+
+    if message[0] == '/':
+        message = update.message.text[1:]
+
     taunt_tuple = get_validated_taunt_tuple(message)
 
     if taunt_tuple:
@@ -23,7 +26,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def inline_taunts(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
-       inline_taunts given a taunt number, functions as a search for the corresponding taunt text.
+       given a taunt number, searches for the corresponding taunt text and audio url
        """
 
     query = update.inline_query.query
@@ -42,7 +45,6 @@ async def inline_taunts(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == "__main__":
-
     token = os.environ.get('BOT_TOKEN')
 
     application = (
@@ -51,11 +53,10 @@ if __name__ == "__main__":
             .build()
     )
 
-    # Handlers
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    text_handler = MessageHandler(filters.TEXT | filters.COMMAND, taunt)  # for messages and commands
     inline_taunts_handler = InlineQueryHandler(inline_taunts)
 
     application.add_handler(inline_taunts_handler)
-    application.add_handler(echo_handler)
+    application.add_handler(text_handler)
 
     application.run_polling()
